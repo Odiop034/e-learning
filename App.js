61299ecb03aa3a39c22630e379236b2921939e07
omar <omardiop2@esp.sn>
2024-06-27 23:41:59 +0000

    Page de connexion,Authentifcation,Tableau de navigation, en tete de l'app réalisés

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import LoginScreen from './App/Screen/LoginScreen';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigation from './App/Navigations/TabNavigation';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-SemiBold.ttf'),
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+    <ClerkProvider 
+    
+    publishableKey={"pk_test_c21hcnQtZ29sZGZpc2gtMzMuY2xlcmsuYWNjb3VudHMuZGV2JA"}>
+      <View style={styles.container}>
+      
+      <SignedIn>
+        
+        <NavigationContainer>
+
+        <TabNavigation/>
+
+        </NavigationContainer>
+
+      </SignedIn>
+      <SignedOut>
+      <LoginScreen/>
+
+      </SignedOut>
     </View>
+    </ClerkProvider>
   );
 }
 
@@ -14,7 +42,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:20
+
   },
 });
